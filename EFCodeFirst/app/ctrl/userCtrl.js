ba3bbfef94e6079b9a82bc675fb76e327ed88f6a
Matt Dunn <mattdunndc@gmail.com>
2016-05-19 16:37:46 -0400

    added svc

---
+++ EFCodeFirst/app/ctrl/userCtrl.js
@@ -0,0 +1,26 @@
+﻿var userCtrl = function ($scope, odataService, notificationFactory) {
+    // Get Top 10 Employees
+    notificationFactory.info('Loading Banks...');
+    $scope.getBanks = function () {
+        (new odataService()).$getTop1000({ entity: 't_inst_institutn' })
+            .then(function (data) {
+
+                $scope.currentPage = 1;
+                $scope.pageSize = 10;
+
+                $scope.banks = data.value;
+
+                $scope.pageChangeHandler = function (num) {
+                    console.log('page changed to ' + num);
+                };
+
+                notificationFactory.success('Banks loaded.');
+            });
+    };
+
+    $scope.sort = function(keyname) {
+        $scope.sortKey = keyname; //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    };
+}
+mod.controller('userCtrl', userCtrl);
---
+++ EFCodeFirst/app/services/services.js
@@ -0,0 +1,30 @@
+﻿mod.factory('odataService', function ($resource) {
+    var odataUrl = 'http://localhost:65406/odata/';
+    return $resource('', {},
+        {
+            'getAll': { method: 'GET', params: { entity: '@entity' }, url: odataUrl },
+            'getTop1000': { method: 'GET',params: { entity: '@entity' }, url: odataUrl + ':entity' + '?$top=1000' },
+            'create': { method: 'POST', url: odataUrl },
+            'patch': { method: 'PATCH', params: { key: '@key' }, url: odataUrl + '(:key)' },
+            'getEntity': { method: 'GET', params: { key: '@key' }, url: odataUrl + '(:key)' },
+            'getEntityAddress': { method: 'GET', params: { key: '@key' }, url: odataUrl + '(:key)' + '/Address' },
+            'getEntityCompany': { method: 'GET', params: { key: '@key' }, url: odataUrl + '(:key)' + '/Company' },
+            'deleteEntity': { method: 'DELETE', params: { key: '@key' }, url: odataUrl + '(:key)' },
+            'addEntity': { method: 'POST', url: odataUrl }
+        });
+}).factory('notificationFactory', function () {
+    return {
+        success: function (text) {
+            toastr.success(text, "Success");
+        },
+        info: function (text) {
+            toastr.info(text, "Info", { "progressBar": true, "newestOnTop": true, "preventDuplicates": true });
+        },
+        warning: function (text) {
+            toastr.warning(text, "Warning");
+        },
+        error: function (text) {
+            toastr.error(text, "Error");
+        }
+    };
+});
